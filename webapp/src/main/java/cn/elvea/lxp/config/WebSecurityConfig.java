bc68584701e4f742025d77d8473f2b57ce3e1398
Elvea Huang <elvea.huang@gmail.com>
2019-09-05 10:04:27 +0800

    暂时屏蔽安全访问控制，完善Spring Boot Admin

--- webapp/src/main/java/cn/elvea/lxp/config/WebSecurityConfig.java
+++ webapp/src/main/java/cn/elvea/lxp/config/WebSecurityConfig.java
@@ -1,14 +1,15 @@
 package cn.elvea.lxp.config;
 
-import cn.elvea.lxp.core.type.RoleType;
-import cn.elvea.lxp.security.*;
+import cn.elvea.lxp.security.SecurityAuthenticationDetailsSource;
+import cn.elvea.lxp.security.SecurityAuthenticationFailureHandler;
+import cn.elvea.lxp.security.SecurityAuthenticationProvider;
+import cn.elvea.lxp.security.SecurityAuthenticationSuccessHandler;
 import cn.elvea.lxp.security.filter.SecurityAuthenticationFilter;
 import cn.elvea.lxp.security.service.SecurityService;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,8 +20,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static cn.elvea.lxp.security.SecurityConstants.API_REQUEST_URL;
 import static cn.elvea.lxp.security.SecurityConstants.XAPI_REQUEST_URL;
 
 /**
@@ -40,11 +41,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private SecurityAuthenticationProvider authenticationProvider;
 
     @Autowired
+    private SecurityAuthenticationDetailsSource securityAuthenticationDetailsSource;
+
+    @Autowired
+    private SecurityAuthenticationFailureHandler authenticationFailureHandler;
+
+    @Autowired
+    private SecurityAuthenticationSuccessHandler authenticationSuccessHandler;
+
+
+    @Override
+    protected AuthenticationManager authenticationManager() {
+        return new ProviderManager(Lists.newArrayList(authenticationProvider));
+    }
+
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider)
                 .userDetailsService(securityService)
                 .passwordEncoder(passwordEncoder());
     }
+
 
     /**
      * 密码加密
@@ -56,81 +73,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * API
-     */
-    @Configuration
-    @Order(1)
-    public static class ApiSecurityConfigAdapter extends WebSecurityBaseConfig {
 
-        @Autowired
-        private SecurityAuthenticationDetailsSource securityAuthenticationDetailsSource;
-
-        @Autowired
-        private SecurityAuthenticationFailureHandler authenticationFailureHandler;
-
-        @Autowired
-        private SecurityAuthenticationSuccessHandler authenticationSuccessHandler;
-
-        @Bean
-        public SecurityAuthenticationFilter securityAuthenticationFilter() {
-            SecurityAuthenticationFilter authenticationFilter = new SecurityAuthenticationFilter();
-            authenticationFilter.setPostOnly(false);
-            authenticationFilter.setAuthenticationManager(authenticationManager());
-            authenticationFilter.setAuthenticationFailureHandler(authenticationFailureHandler);
-            authenticationFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
-            authenticationFilter.setAuthenticationDetailsSource(securityAuthenticationDetailsSource);
-            return authenticationFilter;
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher(SecurityConstants.API_REQUEST_URL)
-                    .addFilterBefore(securityAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                    .csrf().disable()
-                    .cors()
-                    .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .formLogin().disable()
-                    .authorizeRequests()
-                    .antMatchers("/api/*").permitAll()
-                    .antMatchers("/api/user/register").permitAll()
-                    .antMatchers("/api/admin/**").hasAnyRole(RoleType.SYSTEM_ADMINISTRATOR.getCode(), RoleType.ADMINISTRATOR.getCode())
-                    .anyRequest().permitAll();
-        }
+    @Bean
+    public SecurityAuthenticationFilter securityAuthenticationFilter() {
+        SecurityAuthenticationFilter authenticationFilter = new SecurityAuthenticationFilter();
+        authenticationFilter.setPostOnly(false);
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+        authenticationFilter.setAuthenticationFailureHandler(authenticationFailureHandler);
+        authenticationFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+        authenticationFilter.setAuthenticationDetailsSource(securityAuthenticationDetailsSource);
+        return authenticationFilter;
     }
-
-    /**
-     * xAPI
-     */
-    @Configuration
-    @Order(2)
-    public static class XApiSecurityConfigAdapter extends WebSecurityBaseConfig {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.antMatcher(XAPI_REQUEST_URL)
-                    .csrf().disable()
-                    .cors()
-                    .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers(XAPI_REQUEST_URL).permitAll()
-                    .anyRequest().permitAll();
-        }
-    }
-
-}
-
-abstract class WebSecurityBaseConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private SecurityAuthenticationProvider authenticationProvider;
 
     @Override
-    protected AuthenticationManager authenticationManager() {
-        return new ProviderManager(Lists.newArrayList(authenticationProvider));
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests()
+                .antMatchers("/druid/*").permitAll()
+                .antMatchers("/actuator").permitAll()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers(API_REQUEST_URL).permitAll()
+                .antMatchers(XAPI_REQUEST_URL).permitAll()
+                .anyRequest().permitAll();
     }
 
 }
