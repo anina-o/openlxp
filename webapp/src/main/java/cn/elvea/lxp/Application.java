5723c239108718e3c279e9cfcc424f91e1a09897
Elvea Huang <elvea.huang@gmail.com>
2019-10-03 23:48:48 +0800

    忽略Java11下面的反射警告信息

--- webapp/src/main/java/cn/elvea/lxp/Application.java
+++ webapp/src/main/java/cn/elvea/lxp/Application.java
@@ -3,6 +3,9 @@ package cn.elvea.lxp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * Application
  *
@@ -15,7 +18,28 @@ public class Application {
      *
      */
     public static void main(String[] args) {
+        disableAccessWarning();
         SpringApplication.run(Application.class, args);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarning() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
