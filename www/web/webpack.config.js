823a37379a70c59ceaa7859a28b856de890eca61
Elvea Huang <elvea.huang@gmail.com>
2019-10-05 00:27:55 +0800

    前端依赖更新

--- www/web/webpack.config.js
+++ www/web/webpack.config.js
@@ -10,85 +10,85 @@ const env = process.env.environment === "production" ? "production" : "developme
 console.log(`environment - ${env}`);
 
 module.exports = {
-    entry : [
+    entry: [
         './src/index.tsx'
     ],
-    output : {
-        path : path.join(__dirname, 'dist'),
-        filename : '[name].js'
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
     },
-    module : {
-        rules : [{
-            test : /\.ts|\.tsx?$/,
-            exclude : /node_modules/,
-            use : [{
-                loader : 'ts-loader',
-                options : {
-                    transpileOnly : true,
-                    getCustomTransformers : () => ({
-                        before : [tsImportPluginFactory([{
-                            libraryName : 'antd',
-                            libraryDirectory : 'lib',
-                            style : true
+    module: {
+        rules: [{
+            test: /\.ts|\.tsx?$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    getCustomTransformers: () => ({
+                        before: [tsImportPluginFactory([{
+                            libraryName: 'antd',
+                            libraryDirectory: 'lib',
+                            style: true
                         }, {
-                            libraryName : 'antd-mobile',
-                            libraryDirectory : 'lib',
-                            style : true
+                            libraryName: 'antd-mobile',
+                            libraryDirectory: 'lib',
+                            style: true
                         }, {
-                            libraryName : 'lodash',
-                            libraryDirectory : null,
-                            style : false,
-                            camel2DashComponentName : false
+                            libraryName: 'lodash',
+                            libraryDirectory: null,
+                            style: false,
+                            camel2DashComponentName: false
                         }])]
                     })
                 },
             }]
         }, {
-            test : /\.html$/,
-            use : [{
-                loader : "html-loader",
-                options : {
-                    minimize : true
+            test: /\.html$/,
+            use: [{
+                loader: "html-loader",
+                options: {
+                    minimize: true
                 }
             }]
         }, {
-            test : /\.(css|less)$/,
-            use : [{
-                loader : MiniCssExtractPlugin.loader
+            test: /\.(css|less)$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader
             }, {
-                loader : 'css-loader'
+                loader: 'css-loader'
             }, {
-                loader : 'less-loader',
-                options : {
-                    javascriptEnabled : true
+                loader: 'less-loader',
+                options: {
+                    javascriptEnabled: true
                 }
             }]
         }, {
-            test : /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader : 'file-loader'
+            test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'file-loader'
         }, {
-            test : /\.(jpe?g|png|gif|svg)$/i,
-            loader : 'file-loader'
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loader: 'file-loader'
         }]
     },
-    resolve : {
-        extensions : [".ts", ".tsx", ".js", ".jsx", ".json"],
-        alias : {
-            'react-dom' : '@hot-loader/react-dom'
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        alias: {
+            'react-dom': '@hot-loader/react-dom'
         },
-        plugins : [new TSConfigPathsPlugin.TsconfigPathsPlugin({})]
+        plugins: [new TSConfigPathsPlugin.TsconfigPathsPlugin({})]
     },
-    plugins : [
+    plugins: [
         new webpack.NormalModuleReplacementPlugin(
             /src\/environments\/environment\.ts/,
             './environment.' + env + '.ts/'
         ),
         new MiniCssExtractPlugin({
-            filename : '[name].css'
+            filename: '[name].css'
         }),
         new HtmlWebpackPlugin({
-            filename : 'index.html',
-            template : path.resolve(__dirname, './public/index.html')
+            filename: 'index.html',
+            template: path.resolve(__dirname, './public/index.html')
         }),
         new WebpackBundleAnalyzer.BundleAnalyzerPlugin(),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
